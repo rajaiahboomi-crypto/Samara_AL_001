0dd58544d79426a6af46835cc0d317e6605839b0
rajaiahboomi-crypto <rajaiahboomi@gmail.com>
2026-07-31 23:31:29 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+// Samara Care V13. Offline caching remains disabled while live modules are being upgraded.
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',()=>{});
