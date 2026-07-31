17f999d0befc0464b1b70ba8634a176f154c8ade
rajaiahboomi-crypto <rajaiahboomi@gmail.com>
2026-07-31 23:15:23 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.SAMARA_CONFIG = {
+  appName: 'Samara Care',
+  basePath: '/Samara-AL_V7/',
+  supabaseUrl: 'https://askalabwtlrnoodinayq.supabase.co',
+  supabasePublishableKey: 'sb_publishable_MPf0spA1IsJWWR5-ltVAyA_Z_gctBr-',
+  version: '9.0.0'
+};
---
+++ service-worker.js
@@ -0,0 +1,4 @@
+// Samara Care V11 cache-reset release. Offline caching is temporarily disabled.
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',()=>{});
