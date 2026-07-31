4a68f03d9fd2210aa239082b71e8cb8ff92273b3
rajaiahboomi-crypto <rajaiahboomi@gmail.com>
2026-07-31 23:30:03 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-// Samara Care V11 cache-reset release. Offline caching is temporarily disabled.
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',()=>{});
